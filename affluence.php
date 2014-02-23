162cc6d5badc1be8f33274d8bb2f255b2f62ab3d
CPerinet <charles.perinet@gmail.com>
2014-02-23 16:17:48 +0100

    Correction sidebar css

--- affluence.php
+++ affluence.php
@@ -1,4 +1,4 @@
-    <div id="sideba">Constituendi autem sunt qui sint in amicitia fines et quasi termini diligendi. De quibus tres video sententias ferri, quarum nullam probo, unam, ut eodem modo erga amicum adfecti simus, quo erga nosmet ipsos, alteram, ut nostra in amicos benevolentia illorum erga nos benevolentiae pariter aequaliterque respondeat, tertiam, ut, quanti quisque se ipse facit, tanti fiat ab amicis.</div>
+    <div id="sidebar">Constituendi autem sunt qui sint in amicitia fines et quasi termini diligendi. De quibus tres video sententias ferri, quarum nullam probo, unam, ut eodem modo erga amicum adfecti simus, quo erga nosmet ipsos, alteram, ut nostra in amicos benevolentia illorum erga nos benevolentiae pariter aequaliterque respondeat, tertiam, ut, quanti quisque se ipse facit, tanti fiat ab amicis.</div>
 
     <div id="content-side">
             VOUS ETES NANTAIS ET SOUHAITEZ CONNAITRE LE NOMBRE VOTE DE VOTRE BUREAU ?
